9487961070df6903a69ce64a2eb972737257c13f
mandeep-max <tanuverma1977@gmail.com>
2021-10-10 13:39:47 +0530

    add a box object

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    box6 = new Box(980,200,20,20);
 
 }
 
@@ -60,5 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    box6.display();
     platform.display();
 }
